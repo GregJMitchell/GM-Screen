816d4e46f6f99d6700c347511284a2925b41df1a
GregJMitchell <g2mitchell3003@gmail.com>
2021-01-28 15:59:30 -0700

    Import styling

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React from 'react';
-import './App.css';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import HomeView  from "./components/HomeView";
-import  NavBar  from "./components/NavBar";
-import LoginView from './components/LoginView';
-import RegisterView from './components/RegisterView';
-import CampaignsView from './components/CampaignsView';
+import React from 'react'
+import './App.css'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomeView  from "./components/HomeView"
+import  NavBar  from "./components/NavBar"
+import LoginView from './components/LoginView'
+import RegisterView from './components/RegisterView'
+import CampaignsView from './components/CampaignsView'
+import CampaignView from './components/CampaignView'
+import Cookies from "universal-cookie"
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const cookies = new Cookies()
 
 export const NoPageMatch = ({location}) => <h3>No Match for <code>{location.pathname}</code> </h3>
 
@@ -13,15 +18,17 @@ class App extends React.Component {
   renderRoutes = () => {
     return <div>
       <Switch>
-        <Route exact path="/home" component={ HomeView }/>
+        <Route exact path="/" component={ HomeView }/>
         <Route path="/login" component={ LoginView } />
         <Route path="/register" component={ RegisterView } />
-        <Route path="/campaigns" component={ CampaignsView } />
+        <Route exact path="/campaigns" component={ CampaignsView } />
+        <Route path={"/campaigns/:id"} component={ CampaignView } />
         <Route component={ NoPageMatch } />
       </Switch>
     </div>
   }
   render () {
+    console.log(cookies.getAll)
     return (
       <div>
         <Router>
@@ -33,4 +40,4 @@ class App extends React.Component {
   }
 }
 
-export default App;
+export default App
